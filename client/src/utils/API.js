98bf51b570e1f5252117412e76cff3827ae6e660
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-05-02 14:10:20 -0500

    Updating API Key.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default {
     search: function(query, startYear, endYear) {
         const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
-        const APIKEY =  "753f5720889c4bf3b80199c71f4a9288";   
+        const APIKEY =  "ASAALopu1S3IueLj8HvjPntA33ns6H81";   
     
      return axios.get(BASEURL + APIKEY + "&q=" +  query + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231");
     },
